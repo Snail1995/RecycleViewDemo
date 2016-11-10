58a80c961484eb11e02eb0420341089fa2af5abd
Snail1995 <fenghongjing2016@163.com>
2016-11-11 01:03:51 +0800

    RecycleView实现瀑布流的Demo3

--- app/src/main/java/org/paidaxing/recycledemo/model/Item.java
+++ app/src/main/java/org/paidaxing/recycledemo/model/Item.java
@@ -13,13 +13,13 @@ public class Item {
     private String imageUrl;
     private String name;
 
-
     public static Item createItmfromJson(JSONObject json) {
         Item ret = new Item();
         ret.name = json.optString("myname");
         ret.imageUrl = json.optString("smallpic");
         return ret;
     }
+
 
     public String getImageUrl() {
         return imageUrl;
